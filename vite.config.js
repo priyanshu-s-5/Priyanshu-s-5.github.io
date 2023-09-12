8eabb1cd171ae038c909606cb7dd01f326d20816
Subhanjali Sharma <subhanjali.so@gmail.com>
2023-09-13 01:57:35 +0530

    removing stuff

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   //mm
-  base: "https://priyanshu-s-5/priyanshu-s-5.github.io",
+  // base: "https://priyanshu-s-5/priyanshu-s-5.github.io",
   plugins: [react()],
 })
